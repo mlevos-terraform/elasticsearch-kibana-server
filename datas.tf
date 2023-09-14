d5a9013f8510527d8b71896f03e0f65f12884814
mlevos <mathias.levostre@gmail.com>
2023-09-14 21:56:17 +0200

    changefiltre

--- datas.tf
+++ datas.tf
@@ -2,7 +2,7 @@ data "aws_ami_ids" "amazon" {
   owners = ["amazon"]
 
   filter {
-    name   = "ImageLocation"
-    values = ["amazon/*-kernel-*-x86_64"]
+    name   = "name"
+    values = ["al*-ami-*-kernel-*-x86_64"]
   }
 }
